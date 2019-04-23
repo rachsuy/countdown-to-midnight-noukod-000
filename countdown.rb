a67328d7a887830b5bd2d78f883d117c681aeaa6
Rachele Suy <rachelesuy@gmail.com>
2019-04-23 16:42:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,18 +3,17 @@
 def countdown(numbers)
   while number>0
    puts "#{number} SECOND(S)!"
-   sleep 1
+   #sleep 1
    number -=1
   end
    return puts "HAPPY NEW YEAR!"
 end
 countdown(10)
-#def countdown_with_sleep(number) 
- # number =10
-  #while number > 0
-  # sleep 1
- # @  puts "#{number} SECOND(S)!"
- #  number -= 1
- # end
- # return "HAPPY NEW YEAR!"
-#end
+def countdown_with_sleep(number) 
+  while number > 0
+  sleep 1
+   puts "#{number} SECOND(S)!"
+  number -= 1
+ end
+  return "HAPPY NEW YEAR!"
+end
